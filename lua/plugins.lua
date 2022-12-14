12fae79c4c9b01e2429dad386bbd2221bd25f7dc
Vladislav Mironov <imaf6971@gmail.com>
2022-12-14 17:44:11 +0300

    Added treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,14 @@ vim.cmd [[packad packer.nvim]]
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
-	
+
 	use 'ms-jpq/chadtree'
+
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate',
+		config = function()
+			require('plugins.treesitter')
+		end
+	}
 end)
